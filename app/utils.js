c581902e811e3ea8cdab8bac928b555b1f8cadf0
Jose Saldana <jose.saldana@solutionset.com>
2015-01-19 17:03:13 -0500

    WIP: Audio player directive. Improvements to the UI

--- app/utils.js
+++ app/utils.js
@@ -1,16 +1,28 @@
 angular.module('MusicStore.Utils', [])
+
   .directive('audioControl', function() {
     return {
       restrict: 'E',
       replace: true,
-      template: '<div class="audio-player">' + 
+      template: '<div class="audio-player">' +
+                  '<audio src="{{track.previewUrl}}"></audio>' +
                   '<button id="playOrStop" ng-click="playOrStop()">P<button>' +
                 '</div>',
+      scope: {
+        track: '=track'
+      },
       controller: ['$scope', function($scope) {
+        var track = $scope.track
+
         $scope.playOrStop = function() {
-          $scope.playing = true
+          $scope.playing = ! $scope.playing
+
+          //TODO: Implementing HTML5 Audio playing
         }
-      }]
+      }],
+      link: function(scope, elements, attr) {
+        // TO Be finished: 
+      }
     }
-  }) 
-    
+  })
+
--- tests/utils_specs.js
+++ tests/utils_specs.js
@@ -1,31 +1,68 @@
 describe("MusicStore.Utils", function() {
-  var $rootScope
 
   beforeEach(module('MusicStore.Utils'))
-  beforeEach(inject(function(_$rootScope_) {
-    $rootScope = _$rootScope_
-  }))
 
-  describe("audioControl directive", function() {
-    var $compile
+  describe("directive: audio-control", function() {
+    var element, scope
 
-    beforeEach(inject(function(_$compile_) {
-      $compile = _$compile_
+    beforeEach(inject(function($compile, $rootScope) {
+      scope = $rootScope.$new()
+
+      var audioControl = "<audio-control data-track='track'></audio-control>"
+
+      scope.track = { previewUrl: 'albums/al1/al1tr1.mp3' }
+
+      element = $compile(audioControl)(scope)
+      scope.$digest()
     }))
 
-    it("should generate markup for a custom HTML5 audio control", function() {
-      var element = $compile("<audio-control data-src-id='al1-tr1'></audio-control>")($rootScope)
-
-      $rootScope.$digest()
-
-      expect(element.html()).toContain("<button id=\"playOrStop\"")
-      expect($rootScope.playOrStop).not.toBeUndefined()
-
-      //angular.element(element).eq(0)
-      //expect($rootScope.playing).toBe(true)
+    describe("when rendered", function() {
+      it("should generate markup for a custom HTML5 audio control", function() {
+        expect(element.hasClass('audio-player')).toBe(true)
+        expect(element.find('button').length).toBeGreaterThan(0)
+        expect(element.find('audio').attr('src')).toBe('albums/al1/al1tr1.mp3')
+      })
     })
-  })
 
+    describe("when clicking the play button", function() {
+      it("should play the song", function() {
+        element.find('button').click()
+
+        var isolated = angular.element(element).isolateScope()
+
+        expect(isolated.playing).toBe(true)
+
+        var audioEl = element.find('audio').get(0)
+        expect(audioEl.paused).toBe(false)
+        expect(audioEl.ended).toBe(false)
+
+        expect(element.find('button').text()).toEqual('S')
+      })
+
+      xit("should pause the song if is playing", function() {
+
+        var audioEl = element.find('audio').get(0)
+        if(! audioEl.paused && ! audioEl.ended) 
+          audioEl.pause()
+
+        var isolated = angular.element(element).isolateScope()
+
+        expect(isolated.playing).toBe(false)
+        expect(element.find('audio').get(0).paused).toBe(true)
+        expect(element.find('audio').get(0).ended).toBe(false)
+      })
+    })
+
+    describe("when the song has finished", function() {
+      xit("it should indicate the song can be played", function() {
+        var audioEl = element.find('audio').get(0)
+        audioEl.fastSeek(audioEl.duration.toFixed(2))
+
+        expect(element.find('button').text()).toBe("P")
+      })
+    })
+
+  })
 })
 
 
